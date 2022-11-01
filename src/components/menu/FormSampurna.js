8a417ca70f47ec84b8e7af158b542c1bb59ef9f0
Dwi Rizki Oktaviani <dwirizkiokta@gmail.com>
2022-11-01 21:19:23 +0700

    Update Table (Bank)

--- src/components/menu/FormSampurna.js
+++ src/components/menu/FormSampurna.js
@@ -6,7 +6,7 @@ import { Button } from "primereact/button";
 import { Toolbar } from "primereact/toolbar";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { ToggleButton } from "primereact/togglebutton";
+// import { ToggleButton } from "primereact/togglebutton";
 import { CountryService } from "../../service/CountryService";
 
 const Bank = () => {
@@ -26,11 +26,12 @@ const Bank = () => {
     const [businessData, setBusinessData] = useState("");
 
     const [models] = useState(null);
-    const [customers2] = useState([]);
-    const [idFrozen, setIdFrozen] = useState(false);
-    const [product, setProduct] = useState(null);
-    const [setDeleteProductDialog] = useState(false);
-    const [setProductDialog] = useState(false);
+    // const [customers2] = useState([]);
+    // const [idFrozen, setIdFrozen] = useState(false);
+    // const [product, setProduct] = useState(null);
+    // const [products] = useState(null);
+    // const [setDeleteProductDialog] = useState(false);
+    // const [setProductDialog] = useState(false);
 
     const businessSelectItems = [
         { label: "PT. Alam Sampurna Makmur", value: "PT. Alam Sampurna Makmur" },
@@ -75,71 +76,71 @@ const Bank = () => {
         setFilteredCity(filtered);
     };
 
-    const balanceTemplate = (rowData) => {
-        return <span className="text-bold">{formatCurrency(rowData.balance)}</span>;
-    };
+    // const balanceTemplate = (rowData) => {
+    //     return <span className="text-bold">{formatCurrency(rowData.balance)}</span>;
+    // };
 
-    const countryBodyTemplate = (rowData) => {
-        return (
-            <React.Fragment>
-                <img alt="flag" src="assets/demo/images/flags/flag_placeholder.png" className={`flag flag-${rowData.country.code}`} width={30} />
-                <span style={{ marginLeft: ".5em", verticalAlign: "middle" }} className="image-text">
-                    {rowData.country.name}
-                </span>
-            </React.Fragment>
-        );
-    };
+    // const countryBodyTemplate = (rowData) => {
+    //     return (
+    //         <React.Fragment>
+    //             <img alt="flag" src="assets/demo/images/flags/flag_placeholder.png" className={`flag flag-${rowData.country.code}`} width={30} />
+    //             <span style={{ marginLeft: ".5em", verticalAlign: "middle" }} className="image-text">
+    //                 {rowData.country.name}
+    //             </span>
+    //         </React.Fragment>
+    //     );
+    // };
 
-    const representativeBodyTemplate = (rowData) => {
-        const representative = rowData.representative;
-        return (
-            <React.Fragment>
-                <img alt={representative.name} src={`images/avatar/${representative.image}`} onError={(e) => (e.target.src = "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")} width={32} style={{ verticalAlign: "middle" }} />
-                <span style={{ marginLeft: ".5em", verticalAlign: "middle" }} className="image-text">
-                    {representative.name}
-                </span>
-            </React.Fragment>
-        );
-    };
+    // const representativeBodyTemplate = (rowData) => {
+    //     const representative = rowData.representative;
+    //     return (
+    //         <React.Fragment>
+    //             <img alt={representative.name} src={`images/avatar/${representative.image}`} onError={(e) => (e.target.src = "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")} width={32} style={{ verticalAlign: "middle" }} />
+    //             <span style={{ marginLeft: ".5em", verticalAlign: "middle" }} className="image-text">
+    //                 {representative.name}
+    //             </span>
+    //         </React.Fragment>
+    //     );
+    // };
 
-    const dateBodyTemplate = (rowData) => {
-        return formatDate(rowData.date);
-    };
+    // const dateBodyTemplate = (rowData) => {
+    //     return formatDate(rowData.date);
+    // };
 
-    const statusBodyTemplate = (rowData) => {
-        return <span className={`customer-badge status-${rowData.status}`}>{rowData.status}</span>;
-    };
+    // const statusBodyTemplate = (rowData) => {
+    //     return <span className={`customer-badge status-${rowData.status}`}>{rowData.status}</span>;
+    // };
 
-    const formatDate = (value) => {
-        return value.toLocaleDateString("en-US", {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric",
-        });
-    };
+    // const formatDate = (value) => {
+    //     return value.toLocaleDateString("en-US", {
+    //         day: "2-digit",
+    //         month: "2-digit",
+    //         year: "numeric",
+    //     });
+    // };
 
-    const formatCurrency = (value) => {
-        return value.toLocaleString("id-ID", { style: "currency", currency: "IDR" });
-    };
+    // const formatCurrency = (value) => {
+    //     return value.toLocaleString("id-ID", { style: "currency", currency: "IDR" });
+    // };
 
-    const actionBodyTemplate = (rowData) => {
-        return (
-            <React.Fragment>
-                <Button icon="pi pi-pencil" className="p-button-rounded p-button-success p-button-text mb-2" onClick={() => editProduct(rowData)} />
-                <Button icon="pi pi-trash" className="p-button-rounded p-button-warning p-button-text mb-2" onClick={() => confirmDeleteProduct(rowData)} />
-            </React.Fragment>
-        );
-    };
+    // const actionBodyTemplate = (rowData) => {
+    //     return (
+    //         <React.Fragment>
+    //             <Button icon="pi pi-pencil" className="p-button-rounded p-button-success p-button-text mb-2" onClick={() => editProduct(rowData)} />
+    //             <Button icon="pi pi-trash" className="p-button-rounded p-button-warning p-button-text mb-2" onClick={() => confirmDeleteProduct(rowData)} />
+    //         </React.Fragment>
+    //     );
+    // };
 
-    const editProduct = (product) => {
-        setProduct({ ...product });
-        setProductDialog(true);
-    };
+    // const editProduct = (product) => {
+    //     setProduct({ ...product });
+    //     setProductDialog(true);
+    // };
 
-    const confirmDeleteProduct = (product) => {
-        setProduct(product);
-        setDeleteProductDialog(true);
-    };
+    // const confirmDeleteProduct = (product) => {
+    //     setProduct(product);
+    //     setDeleteProductDialog(true);
+    // };
 
     const leftContents = (
         <React.Fragment>
@@ -234,16 +235,37 @@ const Bank = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-6">
+                    {/* <div className="col-6"> */}
+                    <div className="col-12 xl:col-6">
+                        <div className="card">
+                            <h5>Recent Bank</h5>
+                            <DataTable
+                                value={models}
+                                scrollable
+                                scrollHeight="400px"
+                                paginator
+                                rows={10}
+                                rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+                                responsiveLayout="scroll"
+                            >
+                                <Column field="name" header="Name" filter filterPlaceholder="Search by name" sortable style={{ width: "35%" }} />
+                                <Column field="city" header="City" filter filterPlaceholder="Search by city" sortable style={{ width: "35%" }} />
+                                <Column field="district" header="District" filter filterPlaceholder="Search by district" sortable style={{ width: "35%" }} />
+                                <Column field="state" header="State" filter filterPlaceholder="Search by state" sortable style={{ width: "35%" }} />
+                                <Column field="businessunit" header="Business Unit" filter filterPlaceholder="Search by Business Unit" sortable style={{ width: "35%" }} />
+                            </DataTable>
+                        </div>
                         {/* <ToggleButton checked={idFrozen} onChange={(e) => setIdFrozen(e.value)} onIcon="pi pi-lock" offIcon="pi pi-lock-open" onLabel="Unfreeze Id" offLabel="Freeze Id" style={{ width: "10rem" }} /> */}
 
-                        <DataTable value={models} scrollable scrollHeight="400px" paginator rows={10} rowsPerPageOptions={[5, 10, 25, 50, 75, 100]}
+                        {/* <DataTable value={models} scrollable scrollHeight="400px" paginator rows={10} rowsPerPageOptions={[5, 10, 25, 50, 75, 100]}
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products" responsiveLayout="scroll" stripedRows>
                         <Column field="title" header="Title" filter filterPlaceholder="Search by title" style={{ minWidth: "%" }} />
                         <Column field="body" header="Body" headerStyle={{ width: "%" }} filter sortable></Column>
                         <Column body={actionBodyTemplate} exportable={true} style={{ minWidth: "8rem" }}></Column>
-                        </DataTable>
+                        </DataTable> */}
                         {/* <div className="card h-full"></div> */}
                     </div>
                 </div>
